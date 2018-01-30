1e2867dfe0998cb2f6495c9a445e0e477b28b870
“username <1076453997@qq.com>
2018-01-30 18:31:41 +0800

    公告实体接口
    公告实体接口

--- src/main/java/com/hanming/oa/model/Notice.java
+++ src/main/java/com/hanming/oa/model/Notice.java
@@ -1,55 +1,54 @@
 package com.hanming.oa.model;
 
-import java.util.Date;
-
 public class Notice {
-    private Integer id;
+	private Integer id;
 
-    private Integer userid;
+	private Integer userid;
 
-    private String sketch;
+	private String title;
 
-    private String text;
+	private String text;
 
-    private Date date;
+	private String date;
 
-    public Integer getId() {
-        return id;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public Integer getUserid() {
-        return userid;
-    }
+	public Integer getUserid() {
+		return userid;
+	}
 
-    public void setUserid(Integer userid) {
-        this.userid = userid;
-    }
+	public void setUserid(Integer userid) {
+		this.userid = userid;
+	}
 
-    public String getSketch() {
-        return sketch;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public void setSketch(String sketch) {
-        this.sketch = sketch == null ? null : sketch.trim();
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public String getText() {
-        return text;
-    }
+	public String getText() {
+		return text;
+	}
 
-    public void setText(String text) {
-        this.text = text == null ? null : text.trim();
-    }
+	public void setText(String text) {
+		this.text = text;
+	}
 
-    public Date getDate() {
-        return date;
-    }
+	public String getDate() {
+		return date;
+	}
 
-    public void setDate(Date date) {
-        this.date = date;
-    }
+	public void setDate(String date) {
+		this.date = date;
+	}
+
 }
--- src/main/java/com/hanming/oa/model/NoticeDisplay.java
+++ src/main/java/com/hanming/oa/model/NoticeDisplay.java
@@ -1,5 +1,106 @@
 package com.hanming.oa.model;
 
-public class NoticeDisplay {
 
+public class NoticeDisplay {
+	private Integer id;
+
+    private Integer userid;
+
+    private String title;
+
+    private String text;
+
+    private String date;
+    
+    private Integer userId;
+    
+    private String userName;
+    
+    private String headFile;
+    
+    private String role;
+    
+    private String department;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getUserid() {
+		return userid;
+	}
+
+	public void setUserid(Integer userid) {
+		this.userid = userid;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getHeadFile() {
+		return headFile;
+	}
+
+	public void setHeadFile(String headFile) {
+		this.headFile = headFile;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+    
+    
 }
