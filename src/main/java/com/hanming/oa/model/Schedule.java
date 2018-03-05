c0a5398a4856bdd4fdeb9cc69b04a7533ff9c39f
“username <1076453997@qq.com>
2018-03-05 21:26:17 +0800

    日历实体类
    日历实体类

---
+++ src/main/java/com/hanming/oa/model/Schedule.java
@@ -0,0 +1,94 @@
+package com.hanming.oa.model;
+
+public class Schedule {
+    private String id;
+
+    private String title;
+
+    private String startTime;
+
+    private String endTime;
+
+    private String allDay;
+
+    private String color;
+
+    private String userID;
+
+    private String isFinish;
+
+    private String createTime;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+
+	public String getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+
+	public String getAllDay() {
+		return allDay;
+	}
+
+	public void setAllDay(String allDay) {
+		this.allDay = allDay;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getUserID() {
+		return userID;
+	}
+
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
+	public String getIsFinish() {
+		return isFinish;
+	}
+
+	public void setIsFinish(String isFinish) {
+		this.isFinish = isFinish;
+	}
+
+	public String getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+
+}
