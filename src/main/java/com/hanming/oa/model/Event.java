f64bbf37c329aeb4f06399969d1c392106a034e2
“username <1076453997@qq.com>
2018-03-06 21:03:38 +0800

    修改名称
    修改名称

--- src/main/java/com/hanming/oa/model/Event.java
+++ src/main/java/com/hanming/oa/model/Event.java
@@ -31,7 +31,7 @@ public class Event {
 		this.title = title;
 	}
 
-	public String getStartTime() {
+	public String getStart() {
 		return start;
 	}
 
@@ -39,7 +39,7 @@ public class Event {
 		this.start = start;
 	}
 
-	public String getEndTime() {
+	public String getEnd() {
 		return end;
 	}
 
